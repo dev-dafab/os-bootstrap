3d06cd27321e70420725ab4de7ce4b61766fb1d1
fab-du ( batlab) <fab.defab@gmx.de>
2019-07-08 07:22:13 +0200

    clean up

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,14 +1,3 @@
 
 export {parse_depencencies} from "./dependency_parser"
 export {parse_dotfiles_link} from "./dotfiles_parser"
-
-/**
-module.exports = (configFileObj: any) => {
-  return {
-    dotfiles: require("./dependency_parser.ts")(configFileObj.dependencies),
-    dependencies: require("./dotfiles_parser.ts")(
-      configFileObj["dotfiles installation"]
-    )
-  };
-};
- **/
