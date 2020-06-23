f6b59d44febc5d77f5e583a0ee79966a0e37ef3b
Fabrice Dufils S <dev.dafab@gmail.com>
2020-06-23 18:10:25 +0200

    remove gulpfile from cli

--- scripts/cp-file-to-all-package.js
+++ scripts/cp-file-to-all-package.js
@@ -5,23 +5,24 @@ const rootDir = require('app-root-dir');
 
 program.option('-i, --input <file>', 'input files')
  .option('-o, --output <file>', 'output files')
+ .option('-i, --ignore <file>', 'inored modules')
 
 program.parse(process.argv);
 const input = program.input;
 const output = program.output ? program.output : program.input;
 
 const packagesDir = path.resolve(rootDir.get(), 'packages/*');
+const ignoredPackages = program.ignore.split(',').map(e => e.trim());
+    console.log(ignoredPackages);
+
 const inputFile = path.resolve(rootDir.get(), input);
 
-
 const ret = shelljs.ls('-d', packagesDir);
-const dirs = ret.filter(e => typeof e === 'string');
-console.log("============================================");
-console.log("============================================");
+    const dirs = ret.filter(e => typeof e === 'string')
+    .filter(e => !ignoredPackages.includes(e.split('/').pop()));
+
 console.log("============================================");
 console.log(`copy ${inputFile} into submodules`);
 console.log("============================================");
-console.log("============================================");
-console.log("============================================");
-   dirs.forEach(dir => shelljs.cp('-f', inputFile, `${dir}/${output}`));
+dirs.forEach(dir => shelljs.cp('-f', inputFile, `${dir}/${output}`));
 
